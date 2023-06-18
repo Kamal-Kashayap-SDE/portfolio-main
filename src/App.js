ff0a67241d6f991d61a3535c78ed23d8e45719b7
Kamal-Kashayap-SDE <115485467+Kamal-Kashayap-SDE@users.noreply.github.com>
2023-06-19 02:00:38 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
         <Preloader load={load} />
         <div className="App" id={load ? "no-scroll" : "scroll"}>
           <Routes>
-            <Route path='/' element={<Home />}></Route>
+            <Route path='*' element={<Home />}></Route>
             <Route path='/about' element={<About />}></Route>
             <Route path='/project' element={<Project />}></Route>
             <Route path='/contact' element={<Contact />}></Route>
